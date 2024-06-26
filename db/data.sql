8db7fdae56af4e43bf23cdac989ab11809dd9956
Simon Simonsson <simonsson.simon@gmail.com>
2024-06-26 22:26:19 +0200

    Formatting

--- db/data.sql
+++ db/data.sql
@@ -1,10 +1,21 @@
-CREATE OR REPLACE TABLE node AS SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || 'nodes.parquet');
-CREATE OR REPLACE TABLE nodetag AS SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || 'node-tags.parquet');
+-- nodes
+CREATE OR REPLACE TABLE node AS
+	SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || '/nodes.parquet');
+CREATE OR REPLACE TABLE nodetag AS
+	SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || '/node-tags.parquet');
 
-CREATE OR REPLACE TABLE way AS SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || 'ways.parquet');
-CREATE OR REPLACE TABLE waytag AS SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || 'way-tags.parquet');
-CREATE OR REPLACE TABLE waynode AS SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || 'way-nodes.parquet');
+-- ways
+CREATE OR REPLACE TABLE way AS
+	SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || '/ways.parquet');
+CREATE OR REPLACE TABLE waytag AS
+	SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || '/way-tags.parquet');
+CREATE OR REPLACE TABLE waynode AS
+	SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || '/way-nodes.parquet');
 
-CREATE OR REPLACE TABLE rel AS SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || 'relations.parquet');
-CREATE OR REPLACE TABLE reltag AS SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || 'relation-tags.parquet');
-CREATE OR REPLACE TABLE relmem AS SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || 'relation-members.parquet');
+-- relations
+CREATE OR REPLACE TABLE rel AS
+	SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || '/relations.parquet');
+CREATE OR REPLACE TABLE reltag AS
+	SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || '/relation-tags.parquet');
+CREATE OR REPLACE TABLE relmem AS
+	SELECT * FROM read_parquet(getenv('OSM_DATA_DIR') || '/relation-members.parquet');
