f1e3ae0ed3db2767baa508a0ade4ee2784aca80b
Simon Simonsson <simonsson.simon@gmail.com>
2024-06-28 13:32:08 +0200

    Create graph with all highway nodes
    No edges so far.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
-use duckdb::arrow::record_batch::RecordBatch;
-use duckdb::arrow::util::pretty::print_batches;
-use duckdb::{params, Connection, Result};
+use duckdb;
+use petgraph;
 
-use petgraph::algo::{dijkstra, min_spanning_tree};
-use petgraph::data::FromElements;
-use petgraph::dot::{Config, Dot};
-use petgraph::graph::{NodeIndex, UnGraph};
+fn open() -> duckdb::Result<duckdb::Connection> {
+    let conf = duckdb::Config::default()
+        .access_mode(duckdb::AccessMode::ReadOnly)
+        .expect("Invalid config");
+    duckdb::Connection::open_with_flags("atlas.duckdb", conf)
+}
+
+fn graph(c: &duckdb::Connection) -> Result<petgraph::Graph<i64, i64>, duckdb::Error> {
+    // fetch number of nodes that are part of highways
+    let n: usize = c.query_row("SELECT count(*) FROM node", [], |row| row.get(0))?;
+
+    // create graph with capacity
+    let mut g = petgraph::Graph::<i64, i64>::with_capacity(n.into(), n.into());
+
+    // loop over nodes and add them to the graph
+    let mut stmt = c.prepare("SELECT node FROM highway_node")?;
+    let mut rows = stmt.query([])?;
+
+    while let Ok(Some(r)) = rows.next() {
+        g.add_node(r.get(0)?);
+    }
+
+    Ok(g)
+}
 
 fn main() {
-    println!("Hello, world!");
+    println!("Atlas server");
+
+    let con = open().expect("Failed to open database");
+
+    let n: Result<f32, duckdb::Error> =
+        con.query_row("SELECT count(*) FROM node", [], |row| row.get(0));
+    println!("{} million nodes in database", n.expect("lol") / 1e6);
+
+    let g = graph(&con).expect("fooo");
+    println!("{}M nodes in graph", g.node_count() as f32 / 1e6);
 }
