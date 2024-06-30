d8f30c1ab6ade56d4d1dd5399dc9e0d7711f589e
Simon Simonsson <simonsson.simon@gmail.com>
2024-06-30 22:37:07 +0200

    Add edges to graph

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use duckdb;
 use petgraph;
+use std::collections::HashMap;
 
 fn open() -> duckdb::Result<duckdb::Connection> {
     let conf = duckdb::Config::default()
@@ -10,17 +11,37 @@ fn open() -> duckdb::Result<duckdb::Connection> {
 
 fn graph(c: &duckdb::Connection) -> Result<petgraph::Graph<i64, i64>, duckdb::Error> {
     // fetch number of nodes that are part of highways
-    let n: usize = c.query_row("SELECT count(*) FROM node", [], |row| row.get(0))?;
+    let (n, m): (usize, usize) = c.query_row(
+        "SELECT count(DISTINCT node) AS nodes, count(*) AS edges FROM highway_node",
+        [],
+        |row| Ok((row.get(0)?, row.get(1)?)),
+    )?;
 
     // create graph with capacity
-    let mut g = petgraph::Graph::<i64, i64>::with_capacity(n.into(), n.into());
+    let mut g = petgraph::Graph::<i64, i64>::with_capacity(n, m);
+    let mut nix: HashMap<i64, petgraph::graph::NodeIndex> = HashMap::with_capacity(n);
 
     // loop over nodes and add them to the graph
-    let mut stmt = c.prepare("SELECT node FROM highway_node")?;
+    let mut stmt = c.prepare("SELECT DISTINCT node FROM highway_node")?;
     let mut rows = stmt.query([])?;
 
     while let Ok(Some(r)) = rows.next() {
-        g.add_node(r.get(0)?);
+        let node: i64 = r.get(0)?;
+        nix.insert(node, g.add_node(node));
+    }
+
+    // create edges
+    stmt = c.prepare(
+        "SELECT *
+        FROM
+            (SELECT node, lag(node, -1) OVER (PARTITION BY id) AS nxt FROM highway_node)
+        WHERE nxt IS NOT NULL",
+    )?;
+    rows = stmt.query([])?;
+
+    while let Ok(Some(r)) = rows.next() {
+        let (a, b): (i64, i64) = (r.get(0)?, r.get(1)?);
+        g.update_edge(nix[&a], nix[&b], 1);
     }
 
     Ok(g)
@@ -37,4 +58,5 @@ fn main() {
 
     let g = graph(&con).expect("fooo");
     println!("{}M nodes in graph", g.node_count() as f32 / 1e6);
+    println!("{}M edges in graph", g.edge_count() as f32 / 1e6);
 }
